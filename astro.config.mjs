764d5d89976c05506186a907d733939cd8a3eeaa
Todd Gardner <todd@g12solutions.com>
2024-10-23 14:26:41 -0500

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://toddhgardner.github.io/',
   trailingSlash: "never",
   integrations: [mdx(), sitemap()],
   markdown: {
