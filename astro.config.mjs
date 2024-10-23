8a701a01a5c61c952553e57df66b0e08e2613d85
Todd Gardner <todd@g12solutions.com>
2024-10-23 15:16:29 -0500

    try no slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.toddhgardner.com/',
-  trailingSlash: "always",
+  trailingSlash: "never",
   integrations: [mdx(), sitemap()],
   markdown: {
     rehypePlugins: [
