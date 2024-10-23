655de87edd64f4b874fc02c610582dcd4c875190
Todd Gardner <todd@g12solutions.com>
2024-10-23 13:34:48 -0500

    Cleanup

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Todd H. Gardner";
+export const SITE_DESCRIPTION = "I'm Todd. I develop businesses and build software. I'm the President and Cofounder of TrackJS and the Producer of PubConf. I also consult, train, and speak at all kinds of organizations. We should work together! Let me know how I can help.";
