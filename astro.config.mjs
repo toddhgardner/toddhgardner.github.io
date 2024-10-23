45ecf0a9c3090bdbff3f23a0df5aa560e6d0cd43
Todd Gardner <todd@g12solutions.com>
2024-10-23 14:58:12 -0500

    always use trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.toddhgardner.com/',
-  trailingSlash: "never",
+  trailingSlash: "always",
   integrations: [mdx(), sitemap()],
   markdown: {
     rehypePlugins: [
