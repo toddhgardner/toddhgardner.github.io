7226e15c0fb413a221558d26f040e9c1ce44d9bc
Todd Gardner <todd@g12solutions.com>
2025-10-03 15:35:56 -0500

    initial structure of pulse

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,17 +1,39 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
+import { DateTime } from "luxon";
 
 const blog = defineCollection({
-	type: 'content',
-	// Type-check frontmatter using a schema
-	schema: z.object({
-		title: z.string(),
-		description: z.string(),
-		// Transform string to Date object
-		publishedOn: z.coerce.date(),
-		updatedDate: z.coerce.date().optional(),
-		heroImage: z.string().optional(),
-		heroImageUrl: z.string().optional()
-	}),
+  type: "content",
+  // Type-check frontmatter using a schema
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    publishedOn: z.coerce.date(),
+    updatedDate: z.coerce.date().optional(),
+    heroImage: z.string().optional(),
+    heroImageUrl: z.string().optional()
+  }),
 });
 
-export const collections = { blog };
+/**
+ * Pulse
+ * Pulse "Beats" are short, twitter-like updates meant to be streamed in as a
+ * timeline of events.
+ */
+const pulse = defineCollection({
+  type: "content",
+  schema: z.object({
+    type: z.enum(["text"]).default("text"),
+    publishedOn: z.string().transform((str) => {
+      let dt = DateTime.fromISO(str, { zone: "America/Chicago" });
+      if (!dt.isValid) {
+        dt = DateTime.fromFormat(str, "yyyy-MM-dd h:mma", { zone: "America/Chicago" });
+      }
+      return dt.toJSDate();
+    })
+  })
+})
+
+export const collections = {
+  blog,
+  pulse
+};
---
+++ src/content/pulse/2025-10/2025-10-03-hello-world.md
@@ -0,0 +1,5 @@
+---
+publishedOn: 2025-10-03 1:03pm
+---
+
+Hello world!
