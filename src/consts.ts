8a1c6e10d65a904b2bdea0e9e523b1750dc650b9
Todd Gardner <todd@g12solutions.com>
2025-10-16 11:54:08 -0500

    basic about page

--- src/consts.ts
+++ src/consts.ts
@@ -10,5 +10,55 @@ export const AUTHOR_TITLE = "I start things.";
 export const AUTHOR_DESCRIPTION = "Todd H. Gardner builds software that people actually pay for. Todd's on a mission to make simple tools for complicated problems, fight unnecessary complexity, and show that software can actually make money without a VC sugar daddy.";
 import authorImage from "~/images/toddhgardner.jpg";
 export const AUTHOR_IMAGE = authorImage;
+export const AUTHOR_LINKS = [
+  {
+    name: "BlueSky",
+    url: "https://bsky.app/profile/toddhgardner.com"
+  },
+  {
+    name: "Daily.dev",
+    url: "https://app.daily.dev/toddhgardner"
+  },
+  {
+    name: "DevTo",
+    url: "https://dev.to/toddhgardner"
+  },
+  {
+    name: "Facebook",
+    url: "https://www.facebook.com/toddhgardner"
+  },
+  {
+    name: "GitHub",
+    url: "https://github.com/toddhgardner"
+  },
+  {
+    name: "Google Sites",
+    url: "https://sites.google.com/view/toddontheweb/home"
+  },
+  {
+    name: "Hashnode",
+    url: "https://hashnode.com/@toddhgardner"
+  },
+  {
+    name: "Instagram",
+    url: "https://www.instagram.com/toddhgardner/"
+  },
+  {
+    name: "LinkedIn",
+    url: "https://www.linkedin.com/in/toddhgardner/"
+  },
+  {
+    name: "Reddit",
+    url: "https://www.reddit.com/user/toddhgardner/"
+  },
+  {
+    name: "Tumblr",
+    url: "https://www.tumblr.com/blog/toddhgardner"
+  },
+  {
+    name: "YouTube",
+    url: "https://www.youtube.com/@toddhgardner"
+  }
+]
 
 export const CONTACT_LINK = "https://share.hsforms.com/1BT5UlZrcSQyOuEfanQDpwwebqdl";
