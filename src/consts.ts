0230a0cdf49f8dc0e027d7544c36dddf12a1ae84
Todd Gardner <todd@g12solutions.com>
2025-10-16 20:30:57 -0500

    fix og, share images.

--- src/consts.ts
+++ src/consts.ts
@@ -2,7 +2,7 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Todd H. Gardner";
-export const SITE_DESCRIPTION = "Todd H. Gardner builds software that developers actually pay for. He cofounded TrackJS and Request Metrics because the web is broken and slow, and somebody has to help fix it. Then he created CertKit because apparently remembering SSL renewals is hard. Todd's on a mission to make simple tools for complicated problems, fight unnecessary complexity, and prove that software can actually make money without a VC sugar daddy. He thinks your framework is probably overkill and your \"simple\" solution isn't simple enough.";
+export const SITE_DESCRIPTION = "Todd H. Gardner builds software that people actually pay for. Todd's on a mission to make simple tools for complicated problems, fight unnecessary complexity, and show that software can actually make money without a VC sugar daddy.";
 
 export const AUTHOR_NAME = "Todd H. Gardner";
 export const AUTHOR_HANDLE = "toddhgardner";
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,13 +1,4 @@
 import { defineCollection, z } from "astro:content";
-import { DateTime } from "luxon";
-
-function normalizeDate(str: string) : Date {
-  let dt = DateTime.fromISO(str, { zone: "America/Chicago" });
-  if (!dt.isValid) {
-    dt = DateTime.fromFormat(str, "yyyy-MM-dd h:mma", { zone: "America/Chicago" });
-  }
-  return dt.toJSDate();
-}
 
 const blog = defineCollection({
   type: "content",
@@ -15,11 +6,12 @@ const blog = defineCollection({
     title: z.string(),
     description: z.string(),
     publishedOn: z.coerce.date(),
-    updatedOn: z.string().optional().transform((str) => !str ? str : normalizeDate(str)),
+    updatedOn: z.coerce.date().optional(),
     image: image().optional(),
     video: z.object({
       id: z.string()
-    }).optional()
+    }).optional(),
+    tags: z.array(z.string()).optional()
   }),
 });
 
